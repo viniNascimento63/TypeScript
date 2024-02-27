2a5b272a2ed60f529dc44726e51248ee23ef6295
Vinícius Nascimento <vini_sn@outlook.com>
2024-02-27 19:30:51 -0300

    Interface define tipos

---
+++ primeiros-passos/exemplo02.ts
@@ -0,0 +1,13 @@
+// INSERINDO TIPOS
+
+// Interface definindo os tipos
+interface User {
+    name: string;
+    id: number;
+}
+
+// Criando objeto
+const user: User = {
+    name: "Jhon",
+    id: 0
+}
