57ca8f2773c0c4195bb10d96840f70db30fbbdbf
Vinícius Nascimento <vini_sn@outlook.com>
2024-03-12 14:12:02 -0300

    Usando o tsc

---
+++ tsc/exemplo01.js
@@ -0,0 +1,6 @@
+console.log("Hello, World!");
+// This is an industrial-grade general-purpose greeter function:
+function greet(person, date) {
+    console.log("Hello ".concat(person, ", today is ").concat(date, "!"));
+}
+greet('Vinícius', 2023);
---
+++ tsc/exemplo01.ts
@@ -0,0 +1,8 @@
+console.log("Hello, World!");
+
+// This is an industrial-grade general-purpose greeter function:
+function greet(person, date) {
+    console.log(`Hello ${person}, today is ${date}!`);
+}
+
+greet('Vinícius', 2023);
