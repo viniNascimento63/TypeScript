fb83f08b5a6fbaa64a622407339df6544eccf374
Vinícius Nascimento <vini_sn@outlook.com>
2024-03-14 16:59:42 -0300

    erased-types

--- erased-types/exemplo01.js
+++ erased-types/exemplo01.js
@@ -1,5 +1,6 @@
 // O javascript apagou as anotações de tipos do typescript.
-// Também alterou o template string para concatenação.
+// Também alterou o template string para concatenação porque
+// a compilação está sendo feita para o ES3 e não ES6
 
 function greet(person, date) {
     console.log("Hello ".concat(person, ", today is ").concat(date.toDateString(), "!"));
