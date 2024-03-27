d9acb68718c3a945f330bfc9606750b02f71b947
Vinícius Nascimento <vini_sn@outlook.com>
2024-03-27 15:05:29 -0300

    type assertions

---
+++ everyday-types/type-assertions/ex01.ts
@@ -0,0 +1,5 @@
+// TYPE ASSERTIONS
+const myCanvas = document.getElementById('main_canvas') as HTMLCanvasElement;
+
+// angle-bracket syntax
+const myCanvas2 = <HTMLCanvasElement>document.getElementById('main_canvas');
